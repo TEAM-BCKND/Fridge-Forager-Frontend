3297480424883c382275aabc587c401486baa6ba
HighMid <xstar671@gmail.com>
2023-12-18 20:09:02 -0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import SignUp from './components/SignUp.jsx';  
@@ -8,7 +7,8 @@ import Search from '/components/Search';
 
 export default function App() {
     return (
-        <Router>
+        <div>
+            <Router>
             <Header />
             <Routes>
                 <Route path="/" element={<div>Home Page Content</div>} />
@@ -16,7 +16,6 @@ export default function App() {
                 <Route path="/Gallery" element={<div>Gallery? Maybe Idk</div>} />
             </Routes>
         </Router>
-       <div>
         <h1>Your App</h1>
         <Search />
         {/* Other components or content */}
