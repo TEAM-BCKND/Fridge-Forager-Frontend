ece23c3964c50a21c3e4d7950665380ab5a67903
HighMid <xstar671@gmail.com>
2023-12-19 09:29:56 -0500

    Fixed useLogin.js Hooks, ready for use

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown , FormControl, Button, Form} from 'react-bootstrap';
-// import { useLogin } from '../hooks/useLogin';
+import useLogin from '../hooks/useLogin';
 
 
 
@@ -9,17 +9,17 @@ export default function Header(){
 
     const placeHolderImg = 'https://placehold.co/600x400';
 
-    // const {
-    //     email,
-    //     handleEmailChange,
-    //     password,
-    //     handlePasswordChange,
-    //     handleSubmit,
-    // } = useLogin();
+    const {
+        email,
+        handleEmailChange,
+        password,
+        handlePasswordChange,
+        handleSubmit,
+    } = useLogin();
 
     return (
         <Navbar variant="dark" bg="dark" expand="lg">
-            <Navbar.Brand href="#home">
+            <Navbar.Brand href="/">
                 <img src={placeHolderImg} 
                 alt="Logo" 
                 width="180" 
@@ -31,12 +31,12 @@ export default function Header(){
                 <Nav.Link as={Link} to="/signup">SIGN UP</Nav.Link>
                 <Nav.Link as={Link} to="/search">Search</Nav.Link>
             </Nav>
-            <Form className="d-flex">
-            <FormControl type="email" placeholder="Email" className="mr-sm-2"/>
-            <FormControl type="password" placeholder="Password" className="mr-sm-2" />
-                {/* <FormControl type="email" placeholder="Email" className="mr-sm-2" value={email} onChange={handleEmailChange}/>
-                <FormControl type="password" placeholder="Password" className="mr-sm-2" value={password} onChange={handlePasswordChange} /> */}
-                <Button variant="outline-success">Log In</Button>
+            <Form className="d-flex" onSubmit={handleSubmit}>
+            {/* <FormControl type="email" placeholder="Email" className="mr-sm-2"/>
+            <FormControl type="password" placeholder="Password" className="mr-sm-2" /> */}
+            <FormControl type="email" placeholder="Email" className="mr-sm-2" value={email} onChange={handleEmailChange}/>
+            <FormControl type="password" placeholder="Password" className="mr-sm-2" value={password} onChange={handlePasswordChange} />
+                <Button variant="outline-success" type="submit">Log In</Button>
             </Form>
             <Nav>
                 <NavDropdown title="More" id="basic-nav-dropdown" align="end">
