fe6aa6dd5e925da9ff7e0e774a1d1afab64ae3e8
Kaitlin Davis <Kaitlin.A.Davis1996@Gmail.com>
2023-12-20 12:16:02 -0600

    trying redirect path

--- src/components/RenderRecipes.jsx
+++ src/components/RenderRecipes.jsx
@@ -1,21 +1,19 @@
-
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import RecipeList from './RecipeList';
 
-const RenderRecipes = (props) => {
+const RenderRecipes = () => {
   const location = useLocation();
-  const { searchResults } = location.state || {};
+  
+  const searchResults = location.state?.searchResults || [];
 
   return (
     <div>
       <h2>Rendered Recipes</h2>
-      <RecipeList searchResults={props.searchResults || []} />
-      
+      <RecipeList searchResults={searchResults} />
     </div>
   );
 };
 
 export default RenderRecipes;
-
 
