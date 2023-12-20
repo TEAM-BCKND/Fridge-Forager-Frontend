2acf1a16c70a5f70928bfe95f5599f308aa7ea1b
Niles Thompson <dewand.th@gmail.com>
2023-12-20 10:23:45 -0800

    testing query

--- src/components/RenderRecipes.jsx
+++ src/components/RenderRecipes.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import RecipeList from './RecipeList';
 
-const RenderRecipes = (props) => {
+const RenderRecipes = () => {
   const location = useLocation();
   const { searchResults } = location.state || {};
 
   return (
     <div>
       <h2>Rendered Recipes</h2>
-      <RecipeList searchResults={props.searchResults || [0]} />
+      <RecipeList searchResults={searchResults || []} />
       
     </div>
   );
