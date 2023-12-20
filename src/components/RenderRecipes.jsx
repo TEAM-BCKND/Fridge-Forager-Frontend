00d33b91bd9e2cd4561c88823a63793177167d5a
Kaitlin Davis <Kaitlin.A.Davis1996@Gmail.com>
2023-12-20 15:34:09 -0600

    added recipe cards that displays recipes

--- src/components/RenderRecipes.jsx
+++ src/components/RenderRecipes.jsx
@@ -1,19 +1,18 @@
-
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import React from 'react';
 import { useSearchParams } from 'react-router-dom';
-
-
+import './RenderRecipes.css';
 
 const RenderRecipes = () => {
-
-
     const [searchResults, setSearchResults] = useState([]);
     const [searchParams] = useSearchParams();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+
     useEffect(() => {
-        // Function to handle the search
         const handleSearch = async () => {
+            setIsLoading(true);
+            setError('');
             try {
                 const protein = searchParams.get('protein');
                 const vegetable = searchParams.get('vegetable');
@@ -22,35 +21,58 @@ const RenderRecipes = () => {
                 if (protein) query.push(`protein:${protein}`);
                 if (vegetable) query.push(`vegetable:${vegetable}`);
                 if (starch) query.push(`starch:${starch}`);
-                // Join the query terms with commas, and encode them for the URL
                 const queryString = encodeURIComponent(query.join('+'));
                 const apiUrl = `https://fridge-forager-backend.onrender.com/api/edamam-recipes?ingredients=${queryString}`;
-                console.log('Searching recipes with query:', apiUrl);
                 const response = await axios.get(apiUrl);
-                setSearchResults(response.data.recipes); // Assuming the response contains recipe data
-                console.log('data', response.data);
+                
+                console.log('API Response:', response.data);
 
+        if (response.data.recipes && Array.isArray(response.data.recipes.hits)) {
+          const recipes = response.data.recipes.hits.map(hit => hit.recipe);
+          setSearchResults(recipes);
+      } else {
+          setError('No recipes found or the API structure is different than expected.');
+          console.error('API structure is different than expected:', response.data);
+      }
+  } catch (error) {
+      setError('Error searching recipes: ' + error.message);
+      console.error('Error searching recipes:', error);
+  } finally {
+      setIsLoading(false);
+  }
+};
 
-
-            } catch (error) {
-                console.error('Error searching recipes:', error);
-            }
-        };
-        // Call the search function if any of the parameters are present
         if (searchParams.get('protein') || searchParams.get('vegetable') || searchParams.get('starch')) {
             handleSearch();
         }
-    }, []);
+    }, [searchParams]);
+
     return (
         <div>
             <h2>Rendered Recipes</h2>
-
-            <p>
-                {JSON.stringify(searchResults, null, 2)}
-
-            </p>
-
-
+            {isLoading ? (
+                <p>Loading recipes...</p>
+            ) : error ? (
+                <p className="error-message">{error}</p>
+            ) : (
+                <div className="recipes-container">
+                    {searchResults.map((recipe, index) => (
+                        <div key={index} className="recipe-card">
+                            <img src={recipe.image} alt="Recipe" className="recipe-image"/>
+                            <div className="recipe-details">
+                                <h3>{recipe.label}</h3>
+                                <ul>
+                                    {recipe.ingredients.map((ingredient, i) => (
+                                        <li key={i}>
+                                            {ingredient.quantity} {ingredient.measure} {ingredient.food}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
