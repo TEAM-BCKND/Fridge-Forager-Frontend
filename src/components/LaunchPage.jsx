e344090f0de410f47b90b0abdb41567e215c745f
BrettF5 <brettfort@hotmail.com>
2023-12-20 15:04:58 -0800

    Updating Launch Home Page, add cool css styling

--- src/components/LaunchPage.jsx
+++ src/components/LaunchPage.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import backgroundImage from '/img/hotdog.jpg';
 
-function LaunchPage() {
+import React, { useState, useEffect } from 'react';
+import './LaunchPage.css'; // Import the CSS file for styling
+
+const backgroundImages = [
+    '/img/hotdog.jpg',
+    '/img/database-schema.png',
+    '/img/domain-model.png',
+    // Add more image URLs as needed
+];
+
+const LaunchPage = () => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            // Increment the index to switch to the next image
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % backgroundImages.length);
+        }, 10000); // Change the interval as needed (e.g., 5000 milliseconds for 5 seconds)
+
+        // Clear the interval on component unmount
+        return () => clearInterval(intervalId);
+    }, []);
+
     const backgroundStyle = {
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#fff',
+        backgroundImage: `url(${backgroundImages[currentImageIndex]})`,
+        // ... other styles
     };
 
     return (
-        <div style={backgroundStyle}>
-            <h1>Welcome to our Launch Page!</h1>
-            {/* Add any content or links for your launch page */}
+        <div className="launch-page" style={backgroundStyle}>
+            <h1>Welcome to Fridge Forager!</h1>
+            <p>
+                We're excited to introduce our new app that helps you discover amazing recipes with the ingredients you have.
+            </p>
+            <p>
+                Features:
+            </p>
+            <ul>
+                <li>Search for recipes based on your available ingredients.</li>
+                <li>Save your favorite recipes for quick access.</li>
+                <li>Explore a variety of cuisines and cooking styles.</li>
+            </ul>
+            <p>
+                Get started now and turn your kitchen into a culinary adventure!
+            </p>
+            <div>
+                {/* Add any additional links or buttons for users to navigate */}
+                <a href="/signup">Sign Up</a> | <a href="/login">Log In</a>
+            </div>
         </div>
     );
-}
+};
 
 export default LaunchPage;
